3f27ab46736e8df0c6762287d8040e01dd45b61b
martinhorniktkhk <martin.horn@khk.ee>
2019-01-28 15:04:41 +0200

    Koodi korrastamine funktsioonide eraldi failidesse paigutamisega

--- menu/index.php
+++ menu/index.php
@@ -5,77 +5,12 @@ function soodus($hind, $soodusProtsent){
 ?>
 
 <?php
-$praed = array(
-    array(
-        'nimetus' => 'Sealihapada ploomide ja aprikoosiga',
-        'kirjeldus' => 'sealihapada, lisand, salat, leib',
-        'hind' => 2.65),
-    array(
-        'nimetus' => 'Praetud kanakints',
-        'kirjeldus' => 'praetud kana, lisand, salat, leib',
-        'hind' => 2.50),
-    array(
-        'nimetus' => 'Hakklihakaste',
-        'kirjeldus' => 'hakklihakaste, lisand, salat, leib',
-        'hind' => 2.45),
-    array(
-        'nimetus' => 'Kartul, kaste, salat, leib',
-        'kirjeldus' => '',
-        'hind' => 1.38),
-    array(
-        'nimetus' => 'Hakklihakaste 1/2',
-        'kirjeldus' => 'hakklihakaste, lisand, salat, leib',
-        'hind' => 1.30)
-);
-$supid = array(
-    array(
-        'nimetus' => 'Rassolnik',
-        'kirjeldus' => 'supp, hapukoor, leib',
-        'hind' => 1.10),
-    array(
-        'nimetus' => 'Borss',
-        'kirjeldus' => 'supp, hapukoor, leib',
-        'hind' => 1.30)
-);
-$magusad = array(
-    array(
-        'nimetus' => 'Rosinakisell vahukoorega',
-        'kirjeldus' => '',
-        'hind' => 1.05),
-);
-$joogid = array(
-    array(
-        'nimetus' => 'Mahl',
-        'kirjeldus' => '',
-        'hind' => 0.60),
-    array(
-        'nimetus' => 'Morss',
-        'kirjeldus' => '',
-        'hind' => 0.25),
-    array(
-        'nimetus' => 'Piim',
-        'kirjeldus' => '',
-        'hind' => 0.30),
-    array(
-        'nimetus' => 'Keefir',
-        'kirjeldus' => '',
-        'hind' => 0.39)
-);
+require_once 'lib/fnk.php';
+require_once 'menus/menu.php';
+pais();
 ?>
 
-<!DOCTYPE html>
-<html lang="et">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Sookla menuu</title>
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/"
-          crossorigin="anonymous">
-</head>
-<body>
+
 <div class="container-fluid text-center">
     <div class="row">
         <div class="col">
@@ -178,10 +113,6 @@ $joogid = array(
         </div>
     </div>
 </div>
-<!-- Optional JavaScript -->
-<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-</body>
-</html>
+<?php
+jalus();
+?>
