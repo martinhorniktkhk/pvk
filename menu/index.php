2cb1549ba89d08937ee0f79223295b70ff10b02a
martin.horn <martin.horn@khk.ee>
2019-02-07 13:57:11 +0200

    Lisatud raamistiku failid

--- menu/index.php
+++ menu/index.php
@@ -1,20 +1,8 @@
 <?php
-function soodus($hind, $soodusProtsent){
-    return round($hind * ((100 - $soodusProtsent) / 100), 2);
-}
-?>
 
-<?php
-require_once 'lib/fnk.php';
-require_once 'menus/menu.php';
-pais();
+require_once 'conf.php';
+echo '<pre>';
+print_r($sess);
+echo '</pre>';
 
-menu(array(
-    'praed' => $praed,
-    'supid' => $supid,
-    'magus' => $magusad,
-    'joogid' => $joogid
-));
-
-jalus();
 ?>
