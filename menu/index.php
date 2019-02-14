ebd9863c692cd2947295dcf283a61da2853c2e46
martin.horn <martin.horn@khk.ee>
2019-02-14 09:11:40 +0200

    oop menu - rakenduse peamall täidetud lehe põhistruktuuriga

--- menu/index.php
+++ menu/index.php
@@ -1,8 +1,19 @@
 <?php
 
+define('BASE_DIR', './'); // define('BASE_DIR', '../');
 require_once 'conf.php';
-echo '<pre>';
-print_r($sess);
-echo '</pre>';
+
+//echo '<pre>';
+//print_r($sess);
+//echo '</pre>';
+
+$mainTmpl = new Template('main');
+
+$mainTmpl ->set('title','Menu Application');
+
+$mainTmplContent = $mainTmpl->parse();
+echo $mainTmplContent;
+
+
 
 ?>
