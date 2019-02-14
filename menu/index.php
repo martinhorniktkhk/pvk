49f59184e20220c2b6ff9dc236d9ce1bd918d831
martin.horn <martin.horn@khk.ee>
2019-02-14 09:40:15 +0200

    oop menu - lisatud kategooria pealkiri

--- menu/index.php
+++ menu/index.php
@@ -13,6 +13,10 @@ $contentTmpl = new Template('content');
 
 $courseCardTmpl = new Template('course_card');
 
+$courseCardHeaderTmpl = new Template('course_card_header');
+
+$courseCardTmpl->set('course_card_header',$courseCardHeaderTmpl->parse());
+
 $contentTmpl->add('course_cards',$courseCardTmpl->parse());
 $contentTmpl->add('course_cards',$courseCardTmpl->parse());
 $contentTmpl->add('course_cards',$courseCardTmpl->parse());
