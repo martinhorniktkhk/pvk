14510f8f58e9b4a98e1d674ba759767c71e6d40b
martin.horn <martin.horn@khk.ee>
2019-02-14 09:35:37 +0200

    oop menu - lisatud menüü kaardi mall ja peamall täidetud nelja kaardi konteineritega

--- menu/index.php
+++ menu/index.php
@@ -1,19 +1,24 @@
 <?php
-
+////tegelikult peavad olema conf.php failis
 define('BASE_DIR', './'); // define('BASE_DIR', '../');
-require_once 'conf.php';
-
-//echo '<pre>';
-//print_r($sess);
-//echo '</pre>';
-
+require_once(BASE_DIR.'conf.php');
+//
+/*echo '<pre>';
+print_r($sess);
+echo '</pre>';*/
 $mainTmpl = new Template('main');
+$mainTmpl->set('title', 'Menu Application');
 
-$mainTmpl ->set('title','Menu Application');
+$contentTmpl = new Template('content');
+
+$courseCardTmpl = new Template('course_card');
+
+$contentTmpl->add('course_cards',$courseCardTmpl->parse());
+$contentTmpl->add('course_cards',$courseCardTmpl->parse());
+$contentTmpl->add('course_cards',$courseCardTmpl->parse());
+$contentTmpl->add('course_cards',$courseCardTmpl->parse());
+
+$mainTmpl->set('content', $contentTmpl->parse());
 
 $mainTmplContent = $mainTmpl->parse();
 echo $mainTmplContent;
-
-
-
-?>
