8a4858c300834b00ca002dbe8f545ced00a1e90c
martin.horn <martin.horn@khk.ee>
2019-02-21 09:37:53 +0200

    oop menu - menüü andmed on eraldatud põhirakendusest

--- menu/index.php
+++ menu/index.php
@@ -9,38 +9,7 @@ echo '</pre>';*/
 $mainTmpl = new Template('main');
 $mainTmpl->set('title', 'Menu Application');
 $contentTmpl = new Template('content');
-$courses = array(
-    array(
-        'name' => 'praed',
-        'icon' => 'fa-utensils',
-        'data' => array(
-            array(
-                'dish_name' => 'Sealihapada ploomide ja aprikoosiga',
-                'dish_description' => 'sealihapada, lisand, salat, leib',
-                'dish_price' => 2.65,
-                'discount' => 2.25
-            ),
-            array(
-                'dish_name' => 'Praetud kanakints',
-                'dish_description' => 'praetud kana, lisand, salat, leib',
-                'dish_price' => 2.50,
-                'discount' => 2.13
-            ),
-        )
-    ),
-    array(
-        'name' => 'supid',
-        'icon' => 'fa-utensil-spoon',
-        'data' => array(
-            array(
-                'dish_name' => 'Rassolnik',
-                'dish_description' => 'supp, hapukoor, leib',
-                'dish_price' => 1.10,
-                'discount' => 0.94
-            )
-        )
-    )
-);
+require_once 'menus/menu.php';
 
 foreach ($courses as $course => $courseData){
     $courseCardTmpl = new Template('course_card');
