771da29b9967098866ed0e4a7b2014cecdd25863
martin.horn <martin.horn@khk.ee>
2019-02-21 09:33:19 +0200

    oop menu - parandatud andmete lugemine söögi väljastamiseks

--- menu/index.php
+++ menu/index.php
@@ -8,27 +8,60 @@ print_r($sess);
 echo '</pre>';*/
 $mainTmpl = new Template('main');
 $mainTmpl->set('title', 'Menu Application');
-
 $contentTmpl = new Template('content');
+$courses = array(
+    array(
+        'name' => 'praed',
+        'icon' => 'fa-utensils',
+        'data' => array(
+            array(
+                'dish_name' => 'Sealihapada ploomide ja aprikoosiga',
+                'dish_description' => 'sealihapada, lisand, salat, leib',
+                'dish_price' => 2.65,
+                'discount' => 2.25
+            ),
+            array(
+                'dish_name' => 'Praetud kanakints',
+                'dish_description' => 'praetud kana, lisand, salat, leib',
+                'dish_price' => 2.50,
+                'discount' => 2.13
+            ),
+        )
+    ),
+    array(
+        'name' => 'supid',
+        'icon' => 'fa-utensil-spoon',
+        'data' => array(
+            array(
+                'dish_name' => 'Rassolnik',
+                'dish_description' => 'supp, hapukoor, leib',
+                'dish_price' => 1.10,
+                'discount' => 0.94
+            )
+        )
+    )
+);
 
-$courseCardTmpl = new Template('course_card');
+foreach ($courses as $course => $courseData){
+    $courseCardTmpl = new Template('course_card');
+    $courseCardHeaderTmpl = new Template('course_card_header');
+    $courseCardDataTmpl = new Template('course_card_data');
+    $courseCardHeaderTmpl->set('course_name', $courseData['name']);
+    $courseCardHeaderTmpl->set('course_icon', $courseData['icon']);
+    $courseCardTmpl->set('course_card_header', $courseCardHeaderTmpl->parse());
+    $courseCardDataTmpl->set('course_name', $courseData['name']);
+    $courseDatalistTmpl = new Template('course_data_list');
 
-$courseCardHeaderTmpl = new Template('course_card_header');
-$courseCardDataTmpl = new Template('course_card_data');
+    foreach ($courseData['data'] as $dish => $dishData){
+        foreach ($dishData as $name => $value) {
+            $courseDatalistTmpl->set($name, $dishData[$name]);
+        }
+        $courseCardDataTmpl->add('course_data_list', $courseDatalistTmpl->parse());
+    }
 
-$courseNames = array('praed' => 'fa-utensils', 'supid' => 'fa-utensil-spoon', 'magustoidud' => 'fa-cookie-bite', 'joogid' => 'fa-glass-whiskey');
-
-foreach ($courseNames as $courseName => $courseIcon) {
-    $courseCardHeaderTmpl->set('course_name',$courseName);
-    $courseCardHeaderTmpl->set('course_icon',$courseIcon);
-    $courseCardTmpl->set('course_card_header',$courseCardHeaderTmpl->parse());
-
-    $courseCardDataTmpl->set('course_name',$courseName);
-    $courseCardTmpl->set('course_card_data',$courseCardDataTmpl->parse());
-    $contentTmpl->add('course_cards',$courseCardTmpl->parse());
+    $courseCardTmpl->set('course_card_data', $courseCardDataTmpl->parse());
+    $contentTmpl->add('course_cards', $courseCardTmpl->parse());
 }
-
 $mainTmpl->set('content', $contentTmpl->parse());
-
 $mainTmplContent = $mainTmpl->parse();
 echo $mainTmplContent;
